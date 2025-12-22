7f3f3e9de3d393c4d42ceb431b029d911887afc9
ankitghl <gankit27@gmail.com>
2025-12-23 00:52:53 +0530

    adds postgres connection logic in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"Offline-First/internal/db"
+	"log"
+	"os"
+)
+
 func main() {
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL is not set!")
+	}
+
+	conn, err := db.NewPostgres(dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	log.Println("database connected")
 }
